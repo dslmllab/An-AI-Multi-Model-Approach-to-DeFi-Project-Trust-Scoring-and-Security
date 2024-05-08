f51eba83d46c686db4fe73da8da8077dbe3ae767
viraaji <viraaji.m@gmail.com>
2024-05-07 23:57:07 -0400

    code

--- src/getSmartContractData.py
+++ src/getSmartContractData.py
@@ -19,94 +19,54 @@ ftSCAN_API_KEY = getApiKeys.get_api_keys('ftscan')
 
 ftSCAN_API_ENDPOINT = "https://api.bscscan.com/api"
 
-
-def get_contract_details(contract_address):
-    params = {
+def get_contract_info(contract_address):
+    params1 = {
         "module": "contract",
-        "action": "getabi",
+        "action": "getsourcecode",
         "address": contract_address,
         "apikey": ETHERSCAN_API_KEY
     }
-
-    response = requests.get(ETHERSCAN_API_ENDPOINT, params=params)
-    abi_data = response.json()
-
-    params = {
-        "module": "account",
-        #"action": "tokentx",
-        "action": "txlist",
-        "address": contract_address,
-        "startblock": 0,
-        "endblock": 99999999,
-        "sort": "desc",
-        "apikey": ETHERSCAN_API_KEY
-    }
-
-    response = requests.get(ETHERSCAN_API_ENDPOINT, params=params)
-    tx_data = response.json()
-
-    if abi_data["status"] == "1" and tx_data["status"] == "1":
-        contract_abi = abi_data["result"]
-
-        contract_transactions = tx_data["result"]
-
-        return contract_abi, contract_transactions
-    else:
-        return None, None
-
-
-import requests
-import configparser
-import getApiKeys
-
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-
-
-ETHERSCAN_API_KEY = getApiKeys.get_api_keys('ether')
-ETHERSCAN_API_ENDPOINT = "https://api.etherscan.io/api"
-
-BACSCAN_API_KEY = getApiKeys.get_api_keys('bscan')
-
-BACSCAN_API_ENDPOINT = "https://api.bscscan.com/api"
-
-ftSCAN_API_KEY = getApiKeys.get_api_keys('ftscan')
-
-ftSCAN_API_ENDPOINT = "https://api.bscscan.com/api"
-
-
-def get_contract_details(contract_address):
-    params = {
+    params2 = {
         "module": "contract",
-        "action": "getabi",
+        "action": "getsourcecode",
         "address": contract_address,
-        "apikey": ETHERSCAN_API_KEY
+        "apikey": BACSCAN_API_KEY
+    }
+    params3 = {
+        "module": "contract",
+        "action": "getsourcecode",
+        "address": contract_address,
+        "apikey": ftSCAN_API_KEY
     }
 
-    response = requests.get(ETHERSCAN_API_ENDPOINT, params=params)
-    abi_data = response.json()
 
-    params = {
-        "module": "account",
-        #"action": "tokentx",
-        "action": "txlist",
-        "address": contract_address,
-        "startblock": 0,
-        "endblock": 99999999,
-        "sort": "desc",
-        "apikey": ETHERSCAN_API_KEY
-    }
+    response = requests.get(ETHERSCAN_API_ENDPOINT, params=params1)
 
-    response = requests.get(ETHERSCAN_API_ENDPOINT, params=params)
-    tx_data = response.json()
+    data = response.json()
 
-    if abi_data["status"] == "1" and tx_data["status"] == "1":
-        contract_abi = abi_data["result"]
-
-        contract_transactions = tx_data["result"]
-
-        return contract_abi, contract_transactions
+    if data["status"] == "1" :
+        source_code = data["result"][0]["SourceCode"]
+        contract_name = data["result"][0]["ContractName"]
+        compiler_version = data["result"][0]["CompilerVersion"]
+        return source_code, contract_name, compiler_version
     else:
-        return None, None
+        response = requests.get(BACSCAN_API_ENDPOINT, params=params2)
+        data = response.json()
+        print(data)
+        if data["status"] == "1":
+            source_code = data["result"][0]["SourceCode"]
+            contract_name = data["result"][0]["ContractName"]
+            compiler_version = data["result"][0]["CompilerVersion"]
+            return source_code, contract_name, compiler_version
+        else:
+            response = requests.get(ftSCAN_API_KEY, params=params3)
+            data = response.json()
 
+            if data["status"] == "1":
+                source_code = data["result"][0]["SourceCode"]
+                contract_name = data["result"][0]["ContractName"]
+                compiler_version = data["result"][0]["CompilerVersion"]
+                return source_code, contract_name, compiler_version
+
+            else:
+                return None, None, None
---
+++ src/getTransactionData.py
@@ -0,0 +1,56 @@
+
+import requests
+import configparser
+import getApiKeys
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+
+
+ETHERSCAN_API_KEY = getApiKeys.get_api_keys('ether')
+ETHERSCAN_API_ENDPOINT = "https://api.etherscan.io/api"
+
+BACSCAN_API_KEY = getApiKeys.get_api_keys('bscan')
+
+BACSCAN_API_ENDPOINT = "https://api.bscscan.com/api"
+
+ftSCAN_API_KEY = getApiKeys.get_api_keys('ftscan')
+
+ftSCAN_API_ENDPOINT = "https://api.bscscan.com/api"
+
+
+
+def get_contract_details(contract_address):
+    params = {
+        "module": "contract",
+        "action": "getabi",
+        "address": contract_address,
+        "apikey": ETHERSCAN_API_KEY
+    }
+
+    response = requests.get(ETHERSCAN_API_ENDPOINT, params=params)
+    abi_data = response.json()
+
+    params = {
+        "module": "account",
+        #"action": "tokentx",
+        "action": "txlist",
+        "address": contract_address,
+        "startblock": 0,
+        "endblock": 99999999,
+        "sort": "desc",
+        "apikey": ETHERSCAN_API_KEY
+    }
+
+    response = requests.get(ETHERSCAN_API_ENDPOINT, params=params)
+    tx_data = response.json()
+
+    if abi_data["status"] == "1" and tx_data["status"] == "1":
+        contract_abi = abi_data["result"]
+
+        contract_transactions = tx_data["result"]
+
+        return contract_abi, contract_transactions
+    else:
+        return None, None
